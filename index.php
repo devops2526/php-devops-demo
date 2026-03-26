4bf174e4255dbd4c4706c302077bf3f4202afd05
Oluseyi Oyedeji <Oluseyi.Oyedeji@northampton.ac.uk>
2026-03-26 13:10:47 +0000

    Add PHP source files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$environment = "DEV";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP DevOps Demo</title>
+</head>
+<body>
+    <h1>Student Results Portal</h1>
+    <p>Environment: <?php echo $environment; ?></p>
+    <p>Version 1.0</p>
+</body>
+</html>
